d5bce00139e287506bd5f783cbf4f097abbdb31e
grazielle-bastos <grazielle1101@gmail.com>
2026-02-16 19:47:37 -0300

    Includes the entity's version of the DTO and manually maps the entity x DTO paths in NinjaMapper and MissoesMapper

--- src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.java10x.cadastrodeninjas.ninjas;
 
-public class NInjaDTO {
+import dev.java10x.cadastrodeninjas.missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String idade;
+    private String rank;
+    private MissoesModel missoes;
 }
--- src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package dev.java10x.cadastrodeninjas.ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+        }
 }
